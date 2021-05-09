d6d8d25f856b4adffa90988c1ad5e84667775def
sutharketan <ketan.suthar@kuliza.com>
2021-05-09 11:12:48 +0530

    initialized project

---
+++ src/main/java/com/clone/truecaller/TruecallerApplication.java
@@ -0,0 +1,13 @@
+package com.clone.truecaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruecallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruecallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clone/truecaller/TruecallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.truecaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruecallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
